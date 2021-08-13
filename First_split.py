5f96f538935a41dfe822ce02737a4e7fa3d06bde
Logesh. R <56111042+logeshr289@users.noreply.github.com>
2021-08-13 07:58:46 +0530

    Create First_split.py

---
+++ First_split.py
@@ -0,0 +1,13 @@
+import re
+
+string = 'Twelve:12 Eighty nine:89 Nine:9.'
+pattern = '\d+'
+
+# maxsplit = 1
+# split only at the first occurrence
+result = re.split(pattern, string, 1) 
+print(result)
+
+
+#first split
+#['Twelve:', ' Eighty nine:89 Nine:9.']
