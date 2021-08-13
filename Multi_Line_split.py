94fd40c0ab771f4cc4848ab33d7926a616652800
Logesh. R <56111042+logeshr289@users.noreply.github.com>
2021-08-13 08:00:56 +0530

    Create Multi_Line_split.py

---
+++ Multi_Line_split.py
@@ -0,0 +1,15 @@
+import re
+
+# multiline string
+string = 'abc 12\
+de 23 \n f45 6'
+
+pattern = '\s+'
+
+replace = ''
+
+new_string = re.sub(pattern, replace, string) 
+print(new_string)
+
+
+#abc12de23f456
